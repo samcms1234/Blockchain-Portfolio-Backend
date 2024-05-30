e4fd6223c8b227d6b7172a7c58c7459538e5575b
Saumya Srivastava <samdevpro99@gmail.com>
2024-05-30 20:42:02 +0530

    Added API endpoints and tested using Postman

--- index.js
+++ index.js
@@ -9,23 +9,41 @@ const interaction = require('./interaction')
 const PORT = process.env.PORT;
 
 app.get('/resume', async (req, res) => {
-    res.send({'cid' :
-        `${await interaction.fetchResumeCid()}`
-    })
+    try {
+        res.send({'cid' :
+            `${await interaction.fetchResumeCid()}`
+        })
+    }
+    catch(error) {
+        console.log("Unable to fetch");
+        res.send("Unable to fetch");
+    }
 })
 
 app.get('/imagecid', async (req, res) => {
-    res.send({
-        'resumeCid':
-        `${await interaction.fetchImageCid()}`
-    })
+    try {
+        res.send({
+            'resumeCid':
+            `${await interaction.fetchImageCid()}`
+        })
+    }
+    catch(error) {
+        console.log("Unable to fetch");
+        res.send("Unable to fetch");
+    }
 })
 
 app.get('/fetchprofile', async (req, res) => {
-    res.send({
-        'profile':
-        `${await interaction.fetchProfile()}`
-    })
+    try {
+        res.send({
+            'profile':
+            `${await interaction.fetchProfile()}`
+        })
+    }
+    catch(error) {
+        console.log("Unable to fetch");
+        res.send("Unable to fetch");
+    }
 })
 
 app.patch('/changeimage/:cid', async (req, res) => {
@@ -44,7 +62,7 @@ app.patch('/changeimage/:cid', async (req, res) => {
         console.log("Error in changing the image");
 
         res.status(401);
-        res.send("Request failed");
+        res.send({status: "Request failed"});
     }
 })
 
@@ -64,7 +82,7 @@ app.patch('/changeprofile/:text', async (req, res) => {
         console.log("Error in changing the image");
 
         res.status(401);
-        res.send("Request failed");
+        res.send({status: "Request failed"});
     }
 })
 
@@ -84,15 +102,119 @@ app.patch('/changeresume/:cid', async (req, res) => {
         console.log("Error in changing the image");
 
         res.status(401);
-        res.send("Request failed");
+        res.send({status: "Request failed"});
     }
 })
 
 app.use(bodyParser.json());
 
 app.post('/addeducation', async (req, res) => {
-    const { cid } = req.body;
-    res.send(cid);
+    const { collegeName, startingYear, endYear, degree, skillsAcquired, description } = req.body;
+    
+    try {
+
+        console.log("Started Adding Education")
+        await interaction.addEduction(collegeName, startingYear, endYear, degree, skillsAcquired, description);
+
+        console.log("Education successfully added");
+        res.send({status: "Education Successfully Added"})
+    }
+    catch(error) {
+        console.log("Unable to proceed");
+
+        res.send({status: "Unable to add"});
+    }
+})
+
+
+app.patch('/editeducation', async (req, res) => {
+    const { index, collegeName, startingYear, endYear, degree, skillsAcquired, description } = req.body;
+    
+    try {
+
+        console.log("Started Editing Education")
+        await interaction.editEducation(index, collegeName, startingYear, endYear, degree, skillsAcquired, description);
+
+        console.log("Education successfully edited");
+        res.send({status: "Education Successfully Edited"})
+    }
+    catch(error) {
+        console.log("Unable to proceed");
+
+        res.send({status: "Unable to edit"});
+    }
+})
+
+app.get('/geteducation/:index', async (req, res) => {
+    const index = req.params.index;
+
+    try {
+        console.log("fetching the education");
+
+        const Education = await interaction.getEducation(index);
+
+        res.json({collegeName: `${Education.collegeName}`, startingYear: `${Education.startingYear}`, endYear: `${Education.endYear}`, degree: `${Education.degree}`, skillsAcquired: Education.skillsAcquired, description: `${Education.description}`})
+    }
+    catch(error) {
+        console.log("Error in fetching");
+
+        res.send({status: "Unable to fetch"})
+    }
+})
+
+app.post('/addproject', async (req, res) => {
+    const { title, githubLink, description, skillsAcquired, images } = req.body;
+    
+    try {
+
+        console.log("Started Adding Project")
+        await interaction.addProject(title, githubLink, description, skillsAcquired, images);
+
+        console.log("Project successfully added");
+        res.send({status: "Project Successfully Added"})
+    }
+    catch(error) {
+        console.log("Unable to proceed");
+
+        res.send({status: "Unable to add"});
+    }
+})
+
+
+app.patch('/editproject', async (req, res) => {
+    const { index, title, githubLink, description, skillsAcquired, images } = req.body;
+    
+    try {
+
+        console.log("Started Editing Project")
+        await interaction.editProject(index, title, githubLink, description, skillsAcquired, images);
+
+        console.log("Project successfully edited");
+        res.send({status: "Project Successfully Edited"})
+    }
+    catch(error) {
+        console.log("Unable to proceed");
+
+        res.send({status: "Unable to edit"});
+    }
+})
+
+app.get('/getproject/:index', async (req, res) => {
+    const index = req.params.index;
+
+    try {
+        console.log("fetching the Project");
+
+        const project = await interaction.getProject(index);
+        console.log(project);
+
+        res.json({title: `${project.title}`, githubLink: `${project.githubLink}`, description: `${project.description}`, skillsAcquired: `${project.skillsAcquired}`, images: `${project.images}`})
+    }
+    catch(error) {
+        console.log("Error in fetching");
+
+        res.send({status: "Unable to fetch"})
+    }
 })
 
 
