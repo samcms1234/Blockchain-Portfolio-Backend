13d2a17615473f0b81a39eee8fbe99d08bd17eb5
Saumya Srivastava <samdevpro99@gmail.com>
2024-06-16 17:49:25 +0530

    Updated the config files for smart contract and added deployed to bscTestnet

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require('dotenv').config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
+  networks: {
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    bscMainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: `${process.env.BSCSCAN_API_KEY}`
+  }
 };
